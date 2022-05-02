d1833d77bacc19d788b275f81fa83273782a21d0
Iamsunjith <sunjithsubash07@gmail.com>
2022-05-02 13:23:50 +0530

    Proxy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './shared/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'API';
+
+  constructor(private apiService: ApiService) {}
+  ngOnInit(): void {
+    this.apiService.getMethodTest().subscribe((values) => {
+      console.log(values);
+    });
+    this.apiService.getMethodsTest().subscribe((values) => {
+      console.log(values);
+    });
+  }
 }
--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -2,39 +2,56 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   postEmployee(data: any) {
-    return this.http.post<any>("http://localhost:3000/posts", data)
-      .pipe(map((res: any) => {
+    return this.http.post<any>('http://localhost:3000/posts', data).pipe(
+      map((res: any) => {
         return res;
-      }))
+      })
+    );
   }
 
   getEmployee() {
-    return this.http.get<any>("http://localhost:3000/posts")
-      .pipe(map((res: any) => {
+    return this.http.get<any>('http://localhost:3000/posts').pipe(
+      map((res: any) => {
         return res;
-      }))
+      })
+    );
   }
 
   updateEmployee(data: any, id: number) {
-    return this.http.put<any>("http://localhost:3000/posts/" + id, data)
-      .pipe(map((res: any) => {
+    return this.http.put<any>('http://localhost:3000/posts/' + id, data).pipe(
+      map((res: any) => {
         return res;
-      }))
+      })
+    );
   }
 
   deleteEmployee(id: number) {
-    return this.http.delete<any>("http://localhost:3000/posts/" + id)
-      .pipe(map((res: any) => {
+    return this.http.delete<any>('http://localhost:3000/posts/' + id).pipe(
+      map((res: any) => {
         return res;
-      }))
+      })
+    );
+  }
+
+  getMethodsTest() {
+    return this.http.get<any>('/api/user/validateBankToken').pipe(
+      map((res: any) => {
+        return res;
+      })
+    );
+  }
+  getMethodTest() {
+    return this.http.get<any>('/api/v1/customer/getAllCustomers').pipe(
+      map((res: any) => {
+        return res;
+      })
+    );
   }
 }
